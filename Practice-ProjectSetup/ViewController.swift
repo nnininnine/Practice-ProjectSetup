05cfdb728a19d4a72e0475de1d1000d3cc73b471
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-08-22 11:14:37 +0700

    Initial Commit

---
+++ Practice-ProjectSetup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice-ProjectSetup
+//
+//  Created by 7Peaks on 22/8/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
